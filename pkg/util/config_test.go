7d3bc9eccf18c92a66299c4988e8d07aa8f94b13
Dave Franco <davefranco1987@gmail.com>
2018-07-24 23:20:49 -0400

    config test

---
+++ pkg/util/config_test.go
@@ -0,0 +1,91 @@
+package util
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/oracle/oci-go-sdk/common"
+)
+
+func TestUserIsEmpty(t *testing.T) {
+
+	ouser := OciUser{}
+	if !ouser.isEmpty() {
+		t.Error("Error: user previously created is empty")
+	}
+}
+
+func TestUserIsNotEmpty(t *testing.T) {
+
+	ouser := OciUser{
+		User:   "dfranco",
+		Region: "us-ashburn-1",
+	}
+
+	if ouser.isEmpty() {
+		t.Error("Error: user previously created is not empty")
+	}
+}
+
+func TestIsValidPath(t *testing.T) {
+
+	cfg := Config{Path: os.Getenv("HOME")}
+	if !cfg.validPath() {
+		t.Error("Error: Home is always a valid path ;)")
+	}
+}
+
+func TestIsNotValidPath(t *testing.T) {
+	cfg := Config{Path: "/whateverpath"}
+	if cfg.validPath() {
+		t.Error("Error: /whateverpath is no a valid path unless you like to named your path like this")
+	}
+
+}
+
+func TestLoadWithDefault(t *testing.T) {
+	cfg := Config{}
+	config, err := cfg.Load()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if reflect.TypeOf(config) != reflect.TypeOf(common.DefaultConfigProvider()) {
+		t.Error("Error: should be type common.ConfigProvider")
+	}
+}
+
+func TestLoadWithFileNoProfile(t *testing.T) {
+	configLocation := os.Getenv("HOME") + "/.oci/config"
+	commonConfig, err := common.ConfigurationProviderFromFile(configLocation, "")
+	if err != nil {
+		t.Error(err)
+	}
+	cfg := Config{Path: configLocation}
+	config, err := cfg.Load()
+	if err != nil {
+		t.Error(err)
+	}
+	if reflect.TypeOf(config) != reflect.TypeOf(commonConfig) {
+		t.Errorf("Error: should be type %s", reflect.TypeOf(commonConfig).String())
+	}
+
+}
+
+func TestLoadWithFileProfile(t *testing.T) {
+	configLocation := os.Getenv("HOME") + "/.oci/config"
+	commonConfig, err := common.ConfigurationProviderFromFile(configLocation, "")
+	if err != nil {
+		t.Error(err)
+	}
+	cfg := Config{Path: configLocation, Profile: "DEFAULT"}
+	config, err := cfg.Load()
+	if err != nil {
+		t.Error(err)
+	}
+	if reflect.TypeOf(config) != reflect.TypeOf(commonConfig) {
+		t.Errorf("Error: should be type %s", reflect.TypeOf(commonConfig).String())
+	}
+
+}
