06d69706e3fc63dd26b271f71c350fd1ecbcc93d
Dave Franco <davefranco1987@gmail.com>
2020-01-03 16:02:12 -0300

    resource type and create method

--- pkg/terraform/filegen.go
+++ pkg/terraform/filegen.go
@@ -2,6 +2,7 @@ package terraform
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 )
 
@@ -13,15 +14,15 @@ const (
 
 //Tfile generator struct
 type Tfile struct {
-	Provider []TField `json:"provider,omitempty"`
-	Variable []TField `json:"variable,omitempty"`
-	Data     []TField `json:"data,omitempty"`
-	Resource []TField `json:"resource,omitempty"`
-	Output   []TField `json:"output,omitempty"`
+	Provider []Field `json:"provider,omitempty"`
+	Variable []Field `json:"variable,omitempty"`
+	Data     []Field `json:"data,omitempty"`
+	Resource []Field `json:"resource,omitempty"`
+	Output   []Field `json:"output,omitempty"`
 }
 
-//TField to represent fields in terraform resources
-type TField map[string]interface{}
+//Field to represent fields in terraform resources
+type Field map[string]interface{}
 
 //Genfile will generate a tf.json file to be use by terraform
 func (tf Tfile) Genfile() error {
@@ -39,4 +40,52 @@ func (tf Tfile) Genfile() error {
 	jsonFile.Write(tojson)
 
 	return nil
+}
+
+func validResource(resource string) bool {
+
+	var validr = []string{"oci_identity_compartment",
+		"oci_core_vcn",
+		"oci_core_internet_gateway",
+		"oci_core_route_table",
+		"oci_core_subnet"}
+
+	for _, v := range validr {
+		if v == resource {
+			return true
+		}
+	}
+	return false
+}
+
+func validVarTypes(vtype string) bool {
+	var validtypes = []string{"string", "list", ""}
+}
+
+//Var respresents Variable in tf files
+type Var struct {
+	Vname    string
+	Vtype    string
+	Vdefault string
+}
+
+//Resource to identifu different resources in the terraform
+type Resource struct {
+	Rtype  string
+	Rname  string
+	Rfield Field
+}
+
+//Create resource
+func (r *Resource) Create() Field {
+
+	if r.Rtype == "" {
+		log.Fatal("Resource type cannot be empty")
+	}
+
+	if !validResource(r.Rtype) {
+		log.Fatalf("Invalid resource type %v", r.Rtype)
+	}
+
+	return Field{r.Rtype: Field{r.Rname: r.Rfield}}
 }
