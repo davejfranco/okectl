3a1c276a56595c504b9d37db672cb8a51db1ee1c
Dave Franco <davefranco1987@gmail.com>
2020-01-07 18:01:38 -0300

    network resources with create methods and not exportable struct, route resource and some other related resources not ready yet

--- pkg/terraform/network.go
+++ pkg/terraform/network.go
@@ -1,6 +1,7 @@
 package terraform
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -8,38 +9,70 @@ import (
 	"github.com/davejfranco/okectl/pkg/util"
 )
 
+const (
+	subwrkName = "oke-wrk-subnet-quick-"
+	sublbName  = "oke-lb-subnet-quick-"
+	seclistWrk = "oke-wrk-seclist-"
+	seclistlb  = "oke-lb-seclist-"
+	igwname    = "oke-igw-quick-"
+	ngwname    = "oke-ngw-quick-"
+	vcnName    = "oke-vcn-quick-"
+	rtwrk      = "oke-wrk-rt-"
+	rtlb       = "oke-lb-rt-"
+)
+
 //Subnet describe a subnet to be use in vcn
-type Subnet struct {
-	Cidr   string
-	Public bool //
+type subnet struct {
+	cidr          string
+	vcnID         string
+	rtID          string
+	compartmentID string
 }
 
-//Rtable represents a Route Table on a VCN
-type Rtable struct {
-	Subnets []Subnet
-	Routes  []string
+//ValidCIDR will return error if a subnet is not part of the VCN CIDR
+// func (sub subnet) validCIDR(vcnCIDR string) error {
+// 	return nil
+// }
+
+//Create subnet fields
+func (sub subnet) create() (Field, error) {
+
+	if sub.cidr == "" || sub.vcnID == "" || sub.rtID == "" || sub.compartmentID == "" {
+		return Field{}, errors.New("One or more subnet fields are empty")
+	}
+
+	return Field{"vcn_id": sub.vcnID,
+		"cidr_block":     "10.0.1.0/24",
+		"compartment_id": sub.compartmentID}, nil
 }
 
-type Vcn struct {
-	Name         string
-	Cidr         string
-	CompartmenID string
+//Vcn network
+type vcn struct {
+	name          string
+	cidr          string
+	compartmentID string
 }
 
-//Network components
-type Network struct {
-	VcnID   string
-	IgwID   string
-	NgwID   string
-	Cidr    string
-	Rtables []Rtable
-	Subnets []Subnet
+//Create VCN Resource
+func (v vcn) create() (Field, error) {
+
+	if v.name == "" {
+		v.name = vcnName + util.RandomKey()
+	}
+
+	if !v.validVcnCIDR() {
+		return Field{}, errors.New("Invalid CIDR for the VCN")
+	}
+
+	return Field{"cidr_block": v.cidr,
+		"compartment_id": v.compartmentID,
+		"display_name":   v.name}, nil
 }
 
-//validate that CIDR provided is a valid one
-func (vcn Vcn) validCIDR() bool {
+//ValidVcnCIDR that CIDR provided is a valid one
+func (v vcn) validVcnCIDR() bool {
 
-	_, ipv4net, err := net.ParseCIDR(vcn.Cidr)
+	_, ipv4net, err := net.ParseCIDR(v.cidr)
 	if err != nil {
 		log.Fatal(err)
 		return false
@@ -54,11 +87,53 @@ func (vcn Vcn) validCIDR() bool {
 	return true
 }
 
-//ResourceGen will generate tfile resource
-func (vcn Vcn) ResourceGen() TField {
+//Rtable represents a Route Table on a VCN
+type rtable struct {
+	vcnID         string
+	compartmentID string
+	name          string
+	routes        []route
+}
 
-	if vcn.validCIDR() {
-		return TField{"vcn": TField{"cidr_block": vcn.Cidr, "compartment_id": vcn.CompartmenID, "display_name": vcn.Name}}
+type route struct {
+	netIdentityID   string
+	destinationCIDR string
+	destinationType string
+}
+
+//add routes to a given route table
+func (rt *rtable) addRoutes(rts []route) {
+
+	for _, r := range rts {
+		rt.routes = append(rt.routes, r)
 	}
-	return TField{}
+}
+
+func (rt rtable) create() (Field, error) {
+
+	if len(rt.routes) == 0 {
+		return Field{}, errors.New("At least one route should be added to create this field")
+	}
+
+	//Add all routes available
+	var routeRules []Field
+	for _, ro := range rt.routes {
+		ro := Field{"network_entity_id": ro.netIdentityID,
+			"destination":      ro.destinationCIDR,
+			"destination_type": "CIDR_BLOCK"}
+
+		routeRules = append(routeRules, ro)
+	}
+	return Field{"compartment_id": rt.compartmentID,
+		"vcn_id":      rt.vcnID,
+		"route_rules": routeRules}, nil
+}
+
+//Network components
+type network struct {
+	vcnID         vcn
+	publicOnly    bool
+	compartmentID string
+	rtables       []rtable
+	subnets       []subnet
 }
