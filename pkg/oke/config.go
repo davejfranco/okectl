32a0a2fbe224f83b859b1bd1357c31413dd7c7e9
Dave Franco <davefranco1987@gmail.com>
2022-04-10 10:36:38 +0200

    renamed to oci

--- pkg/oke/config.go
+++ pkg/oke/config.go
@@ -1,6 +1,7 @@
 package oke
 
 import (
+	"errors"
 	"os"
 
 	"github.com/oracle/oci-go-sdk/common"
@@ -29,10 +30,9 @@ func (ouser OciUser) isEmpty() bool {
 type Config struct {
 	Path    string
 	Profile string
-	OciUser
 }
 
-func (cfg Config) validPath() bool {
+func (cfg *Config) validPath() bool {
 
 	if _, err := os.Stat(cfg.Path); err == nil {
 		return true
@@ -41,29 +41,21 @@ func (cfg Config) validPath() bool {
 }
 
 //Load creates a condig object to connect to OCI
-func (cfg Config) Load() (common.ConfigurationProvider, error) {
+func (cfg *Config) Load() (common.ConfigurationProvider, error) {
 
-	switch {
-	case cfg.Path != "" && cfg.Profile != "":
-		c, err := common.ConfigurationProviderFromFileWithProfile(
+	if cfg.Path != "" || cfg.Profile != "" {
+		if !cfg.validPath() {
+			return nil, errors.New("Invalid Path to config file")
+		}
+		c := common.CustomProfileConfigProvider(
 			cfg.Path,
 			cfg.Profile,
-			cfg.KeyPassphrasse,
 		)
-		if err != nil {
-			return nil, err
-		}
 		return c, nil
-	case cfg.Path != "" && cfg.Profile == "":
-		c, err := common.ConfigurationProviderFromFile(cfg.Path, cfg.KeyPassphrasse)
-		if err != nil {
-			return nil, err
-		}
-		return c, nil
-	default:
-		return common.DefaultConfigProvider(), nil
-
 	}
+
+	return common.DefaultConfigProvider(), nil
+
 }
 
 //OkeClient provides connection to Container Engine service
