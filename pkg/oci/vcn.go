d40522254402602f8725aad05b16b911190013a5
Dave Franco <davefranco1987@gmail.com>
2022-04-14 18:16:56 +0200

    improve vcn client, create quick net wip

--- pkg/oci/vcn.go
+++ pkg/oci/vcn.go
@@ -10,29 +10,25 @@ import (
 )
 
 var (
-	defaultVcnName        = "okectl_quick_vcn_" + util.RandomInt(6)
-	defaultCIDR    string = "10.0.0.0/16"
+	defaultName        = "okectl_quick"
+	defaultCIDR string = "10.0.0.0/16"
 )
 
-//Vcn to be used con virtual network ops
 type Vcn struct {
-	Client *core.VirtualNetworkClient
-	Ctx    context.Context
+	Client        *core.VirtualNetworkClient
+	CompartmentID string
+	Ctx           context.Context
 }
 
-//getAllVcns returns all vcn created on a given CompartmentID
-/* func (v *Vcn) getAllVcns(compartmentID string) (response []core.Vcn, err error) {
-
-	lvcn := core.ListVcnsRequest{CompartmentId: common.String(compartmentID)}
-	req, err := v.Client.ListVcns(v.Ctx, lvcn)
-	if err != nil {
-		return req.Items, err
-	}
-	return req.Items, nil
-} */
+//Network to be used con virtual network ops
+type Network struct {
+	Name          string
+	CIDR          string
+	CompartmentID string
+}
 
 //DescribeVcn returns vcn details of a given vcnID
-func (v *Vcn) DescribeVcn(vcnID string) (core.Vcn, error) {
+func (v Vcn) DescribeVcn(vcnID string) (core.Vcn, error) {
 
 	req := core.GetVcnRequest{VcnId: common.String(vcnID)}
 	vcnDetail, err := v.Client.GetVcn(v.Ctx, req)
@@ -43,27 +39,17 @@ func (v *Vcn) DescribeVcn(vcnID string) (core.Vcn, error) {
 
 }
 
-//QuickNetworking will deploy a default vcn to be used the an OKE cluster
-//The QuickVCN method will deploy
-//* Virtual Cloud Network (VCN)
-//* Internet Gateway (IG)
-//* NAT Gateway (NAT)
-//* Service Gateway (SGW)
-func (v *Vcn) QuickNetworking(compartmendID string, ctx context.Context) error {
-
-	return errors.New("This is an error")
-}
-
 //CreateVCN will deploy a vcn in a given compartmentID
-func (v *Vcn) CreateVCN(compartmendID string, ctx context.Context) (core.CreateVcnResponse, error) {
+func (v Vcn) Create(net Network) (core.CreateVcnResponse, error) {
 
 	req := core.CreateVcnRequest{CreateVcnDetails: core.CreateVcnDetails{
-		CompartmentId: &compartmendID,
-		CidrBlock:     &defaultCIDR,
-		DisplayName:   &defaultVcnName,
-	}}
+		CompartmentId: &v.CompartmentID,
+		CidrBlock:     &net.CIDR,
+		DisplayName:   &net.Name,
+	},
+	}
 
-	resp, err := v.Client.CreateVcn(ctx, req)
+	resp, err := v.Client.CreateVcn(v.Ctx, req)
 	if err != nil {
 		return core.CreateVcnResponse{}, err
 	}
@@ -71,72 +57,142 @@ func (v *Vcn) CreateVCN(compartmendID string, ctx context.Context) (core.CreateV
 	return resp, nil
 }
 
-func (v *Vcn) addRouteTable(compartmentID, displayName, vcnID string, routes []core.RouteRule, ctx context.Context) (core.CreateRouteTableResponse, error) {
+func (v Vcn) addRouteTable(displayName, vcnID string, routes []core.RouteRule) (core.CreateRouteTableResponse, error) {
 	req := core.CreateRouteTableRequest{
 		CreateRouteTableDetails: core.CreateRouteTableDetails{
-			CompartmentId: &compartmentID,
+			CompartmentId: &v.CompartmentID,
 			RouteRules:    routes,
 			VcnId:         &vcnID,
 			DisplayName:   &displayName,
 		},
 	}
-	resp, err := v.Client.CreateRouteTable(ctx, req)
+	resp, err := v.Client.CreateRouteTable(v.Ctx, req)
 	if err != nil {
 		return core.CreateRouteTableResponse{}, err
 	}
 	return resp, nil
 }
 
-//AddSubnet will create a subnet for a given VCN
-func (v *Vcn) AddSubnet(compartmentID, cidr, displayName, vcnID string, ctx context.Context) (core.CreateSubnetResponse, error) {
-
-	req := core.CreateSubnetRequest{
-		CreateSubnetDetails: core.CreateSubnetDetails{
-			CompartmentId: &compartmentID,
-			CidrBlock:     &cidr,
-			VcnId:         &vcnID,
-			DisplayName:   &displayName,
-		},
-	}
-
-	resp, err := v.Client.CreateSubnet(ctx, req)
-	if err != nil {
-		return core.CreateSubnetResponse{}, err
-	}
-	return resp, nil
-}
-
 //AddInternetGateway will create a IG to allow access from the internet
-func (v *Vcn) AddInternetGateway(compartmentID, vcnID, displayName string, ctx context.Context) (core.CreateInternetGatewayResponse, error) {
+func (v Vcn) AddInternetGateway(vcnID, displayName string) (core.CreateInternetGatewayResponse, error) {
 
 	isenabled := true
 	req := core.CreateInternetGatewayRequest{
 		CreateInternetGatewayDetails: core.CreateInternetGatewayDetails{
-			CompartmentId: &compartmentID,
+			CompartmentId: &v.CompartmentID,
 			IsEnabled:     &isenabled,
 			VcnId:         &vcnID,
 			DisplayName:   &displayName,
 		},
 	}
-	resp, err := v.Client.CreateInternetGateway(ctx, req)
+	resp, err := v.Client.CreateInternetGateway(v.Ctx, req)
 	if err != nil {
 		return core.CreateInternetGatewayResponse{}, err
 	}
 	return resp, nil
 }
 
-func (v *Vcn) addSecurityList(compartmentID, vcnID string, egressRules []core.EgressSecurityRule, ingressRules []core.IngressSecurityRule, ctx context.Context) (core.CreateSecurityListResponse, error) {
+type SecurityList struct {
+	VcnID       string
+	EgressRule  []core.EgressSecurityRule
+	IngressRule []core.IngressSecurityRule
+}
+
+func (v Vcn) addSecurityList(sl SecurityList) (core.CreateSecurityListResponse, error) {
 	req := core.CreateSecurityListRequest{
 		CreateSecurityListDetails: core.CreateSecurityListDetails{
-			CompartmentId:        &compartmentID,
-			VcnId:                &vcnID,
-			EgressSecurityRules:  egressRules,
-			IngressSecurityRules: ingressRules,
+			CompartmentId:        &v.CompartmentID,
+			VcnId:                &sl.VcnID,
+			EgressSecurityRules:  sl.EgressRule,
+			IngressSecurityRules: sl.IngressRule,
 		},
 	}
-	resp, err := v.Client.CreateSecurityList(ctx, req)
+	resp, err := v.Client.CreateSecurityList(v.Ctx, req)
 	if err != nil {
 		return core.CreateSecurityListResponse{}, nil
 	}
 	return resp, nil
+}
+
+//AddSubnet will create a subnet for a given VCN
+func (v Vcn) AddSubnet(vcnID string, subnet Network) (core.CreateSubnetResponse, error) {
+
+	req := core.CreateSubnetRequest{
+		CreateSubnetDetails: core.CreateSubnetDetails{
+			CompartmentId: &v.CompartmentID,
+			CidrBlock:     &subnet.CIDR,
+			VcnId:         &vcnID,
+			DisplayName:   &subnet.Name,
+		},
+	}
+
+	resp, err := v.Client.CreateSubnet(v.Ctx, req)
+	if err != nil {
+		return core.CreateSubnetResponse{}, err
+	}
+	return resp, nil
+}
+
+//QuickNetworking will deploy a default vcn to be used the an OKE cluster
+//The QuickVCN method will deploy
+//* Virtual Cloud Network (VCN)
+//* Internet Gateway (IG)
+//* NAT Gateway (NAT)
+//* Service Gateway (SGW)
+func QuickNetworking(vcn Vcn) error {
+
+	//VCN First
+	random := util.RandomInt(6)
+	net := Network{
+		Name:          defaultName + "_vcn_" + random,
+		CIDR:          defaultCIDR,
+		CompartmentID: vcn.CompartmentID,
+	}
+	vcnresp, err := vcn.Create(net)
+	if err != nil {
+		return err
+	}
+
+	var quickSubnets []Network
+
+	nodeSubnet := Network{
+		Name:          defaultName + "_subnet_workers_" + random,
+		CIDR:          "10.0.10.0/24",
+		CompartmentID: vcn.CompartmentID,
+	}
+
+	quickSubnets = append(quickSubnets, nodeSubnet)
+
+	svclbSubnet := Network{
+		Name:          defaultName + "_subnet_svclb_" + random,
+		CIDR:          "10.0.20.0/24",
+		CompartmentID: vcn.CompartmentID,
+	}
+
+	quickSubnets = append(quickSubnets, svclbSubnet)
+
+	k8sApiSubnet := Network{
+		Name:          defaultName + "_subnet_k8sApiEndpoint_" + random,
+		CIDR:          "10.0.0.0/28",
+		CompartmentID: vcn.CompartmentID,
+	}
+
+	quickSubnets = append(quickSubnets, k8sApiSubnet)
+
+	//Create three subnets for worker nodes, public services, and k8s api endpoint
+	for _, subnet := range quickSubnets {
+		_, err = vcn.AddSubnet(*vcnresp.Id, subnet)
+		if err != nil {
+			return err
+		}
+	}
+
+	//Create Internet Gateway
+	igwName := defaultName + "_igw_" + random
+	_, err = vcn.AddInternetGateway(*vcnresp.Id, igwName)
+	if err != nil {
+		return err
+	}
+
+	return errors.New("This is an error")
 }
