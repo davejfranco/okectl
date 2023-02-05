c8393bd3a790e06d93585ec96300987d0ee3f8d5
Dave <davefranco1987@gmail.com>
2023-02-05 17:51:30 +0100

    barebone cmd

---
+++ cmd/create/create.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package create
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var CreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create cluster resources",
+	Long:  `deploy a new cluster or create a new resource in an existing cluster`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	//rootCmd.AddCommand(createCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
